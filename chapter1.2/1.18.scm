b595f7d3cf8915840935768b9b16bc578ad1debd
JasonSJ <xjtusunjie@gmail.com>
2020-01-16 23:33:14 +0800

    profile 1.18

--- chapter1.2/1.18.scm
+++ chapter1.2/1.18.scm
@@ -1,13 +1,13 @@
 (define (fast-multi a b)
     (define (double x) (+ x x))
     (define (halve x) (/ x 2))
-    (define (iter product a b counter)
-        (cond ((= counter 0) product)
-            ((even? b) (iter product (double a) (halve b) (halve b)))
-            (else (iter (+ product a) a b (- counter 1)))))
-    (iter 0 a b b))
+    (define (iter product a b)
+        (cond ((= b 0) product)
+            ((even? b) (iter product (double a) (halve b)))
+            (else (iter (+ product a) a (- b 1)))))
+    (iter 0 a b))
 
 
 
 
-(fast-multi 3 4)
+(fast-multi 10 411)
